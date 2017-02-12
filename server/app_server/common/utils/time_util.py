0026421b99c94c7a4df6bd710404ff0150b74fb9
bdh1011 <bdh931101@gmail.com>
2017-02-12 16:17:58 +0900

    Fix timeutil

--- server/app_server/common/utils/time_util.py
+++ server/app_server/common/utils/time_util.py
@@ -3,6 +3,6 @@ from datetime import datetime
 def str_to_datetime(datetime_str):
     """ dump string of datetime to datetime object"""
     try:
-        return datetime.strptime(datetime_str, "%Y/%d/%m %H:%M:%S")
+        return datetime.strptime(datetime_str, "%Y/%m/%d %H:%M:%S")
     except:
         return None
